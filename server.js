c16a3e906ab77058d0887c3f98471fda63915a52
Amy Schillerstrom <amyschillerstrom@Amys-MacBook-Pro.local>
2018-04-24 19:51:29 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,76 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var reservations = [{
+  name: "Amy",
+  phone: 1234,
+  email: "astwefs",
+  unique: "amys"
+
+}];
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+
+
+
+// Create New Reservations - takes in JSON input
+app.post("/api/reservations", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newReservation = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newReservation.Name = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newReservation.slice(0,4));
+
+  reservations.push(newReservation.slice(0,4));
+
+  res.json(newReservation.slice(0,4));
+});
+
+//create waitlist - use slice (5, reservations.length)
+app.post("/api/waitlist", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var waitlist = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  waitlist.Name = waitlist.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(waitlist.slice(5, reservations.length));
+
+  reservations.push(waitlist.slice(5, reservations.length));
+
+  res.json(waitlist.slice(5, reservations.length));
+});
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
